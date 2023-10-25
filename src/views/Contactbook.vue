f4b958da6b471eee9f7d4d3cea9c3762b58b61a4
nvhao96 <nvhao182@gmail.com>
2023-10-25 18:18:22 +0700

    Tao trang cap nhat lien he

--- src/views/Contactbook.vue
+++ src/views/Contactbook.vue
@@ -8,9 +8,7 @@
         Danh bạ
         <i class="fas fa-address-book"></i>
       </h4>
-      <ContactList v-if="filteredContactsCount > 0" 
-        :contacts="filteredContacts" 
-        v-model:activeIndex="activeIndex"/>
+      <ContactList v-if="filteredContactsCount > 0" :contacts="filteredContacts" v-model:activeIndex="activeIndex" />
       <p v-else>Không có liên hệ nào.</p>
       <div class="mt-3 row justify-content-around align-items-center">
         <button class="btn btn-sm btn-primary" @click="refreshList()">
@@ -31,6 +29,13 @@
           <i class="fas fa-address-card"></i>
         </h4>
         <ContactCard :contact="activeContact" />
+        <router-link :to="{
+          name: 'contact.edit',
+          params: { id: activeContact._id },
+        }">
+          <span class="mt-2 badge badge-warning">
+            <i class="fas fa-edit"></i> Hiệu chỉnh</span>
+        </router-link>
       </div>
     </div>
   </div>
@@ -111,7 +116,7 @@ export default {
     mounted() {
       this.refreshList();
     },
-  }, 
+  },
 };
 </script>
 <style scoped>
